eb4f714d54f4b934cc414369c023a9b21c0df9a3
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-10-21 11:43:33 +0300

    Add project files.

---
+++ mongoTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace mongoTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+
+        private IMongoCollection<Shipwreck> _shipwreckCollection;
+
+        public WeatherForecastController(IMongoClient client)
+        {
+            var database = client.GetDatabase("sample_geospatial");
+            _shipwreckCollection = database.GetCollection<Shipwreck>("shipwrecks");
+        }
+
+        [HttpGet]
+        public IEnumerable<Shipwreck> Get()
+        {
+            return _shipwreckCollection.Find(_ =>true).ToList();
+
+
+
+        }
+    }
+}
---
+++ mongoTest/Shipwreck.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace mongoTest
+{
+    [BsonIgnoreExtraElements]
+    public class Shipwreck
+    {
+        public ObjectId Id { get; set; }
+        [BsonElement("feature_type")]
+        public string FeatureType { get; set; }
+        [BsonElement("chart")]
+        public string Chart { get; set; }
+        [BsonElement("latdec")]
+        public double Latitude { get; set; }
+        [BsonElement("londec")]
+        public double Longtitude { get; set;}
+    }
+}
